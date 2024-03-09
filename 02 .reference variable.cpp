267f11e7498ef3e189999a81783de898ff520fc2
Mst:Sumi khatun <155958875+sumihstu@users.noreply.github.com>
2024-03-10 01:30:44 +0600

    Add files via upload

---
+++ 02 .reference variable.cpp
@@ -0,0 +1,16 @@
+
+
+
+//code by sumi(2202130)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int k = 5;//declare variable
+    int &p=k;//declare a reference variable
+    cout<<"k = "<<k<<endl;
+    cout<<"reference of k is p and the value is = "<<p<<endl;//now p is 5
+    return 0;
+
+}
