a66e52a6cb535ba66d991f7cad61c19916483fd6
Mst:Sumi khatun <155958875+sumihstu@users.noreply.github.com>
2024-03-10 01:30:20 +0600

    Add files via upload

---
+++ 01.Basic c++programming.cpp
@@ -0,0 +1,17 @@
+//code by sumi
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string name; // declaring a string type variable for input
+
+    cout << "Enter your  name: ";
+    getline(cin, name);
+
+    cout<<"My name is:"<<endl;
+
+    return 0;
+}
+
