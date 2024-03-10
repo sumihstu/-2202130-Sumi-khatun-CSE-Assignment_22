2210022fd7a3117499b6d6f021ee34b15babac9b
Mst:Sumi khatun <155958875+sumihstu@users.noreply.github.com>
2024-03-10 12:21:27 +0600

    Add files via upload

---
+++ 15(x).use of default argument with clas and object.cpp
@@ -0,0 +1,24 @@
+//code by sumi
+#include<iostream>
+#include<math.h>
+using namespace std;
+class S{
+public:
+    int x;
+    void input(void)
+    {
+       cout<<"Enter the value = ";
+       cin>>x;
+    }
+    void display(void)
+    {
+        cout<<"log(x) = "<<log(x);
+    }
+    };
+int main()
+{
+    S u;
+    u.input();
+    u.display();
+    return 0;
+}
